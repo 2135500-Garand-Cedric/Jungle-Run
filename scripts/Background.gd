3b264657abbeea1884ef9395719042c7a86e0d59
cedric <2135500@carrefour.cegepvicto.ca>
2022-11-24 13:38:19 -0500

    Creation de la scene de background qui defile

---
+++ scripts/Background.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+
+export var startPosY = 500
+export var speed = 10
+
+onready var bg = [$Sprite, $Sprite2, $Sprite3]
+
+var startPosX = 4000
+
+func _ready():
+	var posX = startPosX
+	var posY = startPosY
+	
+	for screen in bg:
+		screen.position.x = posX
+		screen.position.y = posY
+		posX -= 2000
+		
+
+func _physics_process(delta):
+	for screen in bg:
+		screen.position.x -= speed
+		if screen.position.x <= -2000:
+			screen.position.x = startPosX 
