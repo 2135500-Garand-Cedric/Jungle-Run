36812a9d92154a9f48e9bf6725ba9e29565479be
cedric <2135500@carrefour.cegepvicto.ca>
2022-12-01 14:04:21 -0500

    update mineurs

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,11 +2,14 @@ extends KinematicBody2D
 
 # variable déterminant la vitesse
 # exportée pour pouvoir la modifier dans l'éditeur
-export var speed : int = 300
+export var speed = 500.0
+export var gravity = 250.0
+var plateforme = 3
 # variables pour instancier les armes
 var Shuriken = preload("res://scenes/Shuriken.tscn")
 # variable pour espacer les tirs
-var cannonsReady = true
+
+onready var plateformes = []
 
 # variable contenant le vecteur final de déplacement
 var velocity = Vector2();
@@ -14,12 +17,15 @@ var velocity = Vector2();
 
 func _physics_process(delta):
 	# gestion des entrées de l'utilisateur
+	velocity.x = 0
 	getInput()
 	# déplacement
-	var collision = move_and_collide(velocity * speed * delta)
-	# contact avec une balle ou un ennemi
-	if collision:
-		$Visual.play("dead")
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+	for i in get_slide_count():
+		var collision = get_slide_collision(i)
+		if collision.collider.name != "Plateforme" and collision.collider.name != "Plateforme2" and collision.collider.name != "Plateforme3" and collision.collider.name != "Plateforme4":
+			$Visual.play("dead")
 	if $Visual.animation == "dead" and $Visual.frame == 4:
 		$Visual.playing = false
 		$Visual.frame = 4
@@ -32,13 +38,19 @@ func _physics_process(delta):
 # 
 # Selon les entrées de l'utilisateur
 # - modifie les variable de déplacement 
-	velocity = Vector2()
+func getInput():
+	if Input.is_action_pressed("left"):
+		velocity.x += -speed
+		$Visual.flip_h = true
+	if Input.is_action_pressed("right"):
+		velocity.x += speed
+		$Visual.flip_h = false
 	if Input.is_action_just_pressed("shoot"):
 		var bullet = Shuriken.instance()
 		bullet._shoot(position)
 		get_parent().add_child(bullet)
 	elif Input.is_action_just_pressed("up"):
+		
 		$Visual.play("jump")
 	elif Input.is_action_just_pressed("down"):
 		$Visual.play("jump")
-	velocity = velocity.normalized()
